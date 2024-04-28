2fcb8b08d2287d5ae26ec6cd67f10de1cfdb2916
Tanmoy paul <84624966+tanmoypaul1005@users.noreply.github.com>
2024-04-28 22:46:51 +0600

    Refactor navigation bar in RecipeDetails component

--- app/recipe/[recipe_id]/page.js
+++ app/recipe/[recipe_id]/page.js
@@ -1,36 +1,6 @@
 const RecipeDetails = () => {
   return (
     <body>
-      <nav>
-        <div className="container flex justify-between py-6">
-          <a href="index.html">
-            <img
-              src="./assets/images/logo.png"
-              alt=""
-              className="object-cover h-[40px]"
-            />
-          </a>
-
-          <ul className="flex gap-4 text-sm text-gray-500">
-            <li className="py-2 active">
-              <a href="./index.html">Home</a>
-            </li>
-
-            <li className="py-2">
-              <a href="./index.html">Recipe</a>
-            </li>
-
-            <li className="py-2">
-              <a href="./index.html">About us</a>
-            </li>
-
-            <li className="py-2 bg-[#eb4a36] px-6 rounded-md text-white content-center">
-              <a href="./login.html">Login</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-
       <main>
         <section>
           <div className="container grid grid-cols-12 gap-8 justify-items-center">

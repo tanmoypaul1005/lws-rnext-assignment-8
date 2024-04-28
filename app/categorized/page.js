ad9a9a9d63d9d656168bc2c08e8307d9484bad1c
Tanmoy paul <84624966+tanmoypaul1005@users.noreply.github.com>
2024-04-28 22:48:19 +0600

    Refactor Categorized component to use RecipeCard component for displaying recipe cards

--- app/categorized/page.js
+++ app/categorized/page.js
@@ -2,54 +2,18 @@ import RecipeCard from "../components/RecipeCard";
 
 const Categorized = () => {
   return (
-    <div>
-      <body>
-        <nav>
-          <div className="container flex justify-between py-6">
-            <a href="index.html">
-              <img
-                src="./assets/images/logo.png"
-                alt=""
-                className="object-cover h-[40px]"
-              />
-            </a>
+      <section className="container py-8">
+        <div>
+          <h3 className="text-xl font-semibold">Appetizers & Snacks</h3>
 
-            <ul className="flex gap-4 text-sm text-gray-500">
-              <li className="py-2 active">
-                <a href="./index.html">Home</a>
-              </li>
-
-              <li className="py-2">
-                <a href="./index.html">Recipe</a>
-              </li>
-
-              <li className="py-2">
-                <a href="./index.html">About us</a>
-              </li>
-
-              <li className="py-2 bg-[#eb4a36] px-6 rounded-md text-white content-center">
-                <a href="./login.html">Login</a>
-              </li>
-            </ul>
+          <div className="grid grid-cols-1 gap-6 my-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center">
+            <RecipeCard />
+            <RecipeCard />
+            <RecipeCard />
+            <RecipeCard />
           </div>
-        </nav>
-
-        <main>
-          <section className="container py-8">
-            <div>
-              <h3 className="text-xl font-semibold">Appetizers & Snacks</h3>
-
-              <div className="grid grid-cols-1 gap-6 my-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center">
-                <RecipeCard />
-                <RecipeCard />
-                <RecipeCard />
-                <RecipeCard />
-              </div>
-            </div>
-          </section>
-        </main>
-      </body>
-    </div>
+        </div>
+      </section>
   );
 };
 

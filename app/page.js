ea7da1f9b3a0e413b24ef71dd5ea3271518931ec
Tanmoy paul <84624966+tanmoypaul1005@users.noreply.github.com>
2024-04-28 21:21:28 +0600

    first commit

--- app/page.js
+++ app/page.js
@@ -2,112 +2,168 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div>
+      <body>
+        <nav>
+          <div className="container flex justify-between py-6">
+            <a href="index.html">
+              <img
+                src="./assets/images/logo.png"
+                alt=""
+                className="object-cover h-[40px]"
+              />
+            </a>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+            <ul className="flex gap-4 text-sm text-gray-500">
+              <li className="py-2 active">
+                <a href="./index.html">Home</a>
+              </li>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+              <li className="py-2">
+                <a href="./index.html">Recipe</a>
+              </li>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+              <li className="py-2">
+                <a href="./index.html">About us</a>
+              </li>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+              <li className="py-2 bg-[#eb4a36] px-6 rounded-md text-white content-center">
+                <a href="./login.html">Login</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <main>
+          <section className="container">
+            <div
+              style={{
+                backgroundImage: "url('./assets/images/cover.png')",
+                padding: "1rem",
+                borderRadius: "1rem",
+                minHeight: "450px",
+                display: "grid",
+                alignItems: "center",
+                gridTemplateColumns: "repeat(12, 1fr)",
+              }}
+            >
+              <div style={{ gridColumn: "span 12/span 6" }}>
+                <h1
+                  style={{
+                    fontWeight: "bold",
+                    fontSize: "3rem",
+                    color: "white",
+                  }}
+                >
+                  Choose from thousands of recipes
+                </h1>
+                <p style={{ color: "white", margin: "1rem 0" }}>
+                  Appropriately integrate technically sound value with scalable
+                  infomediaries negotiate sustainable strategic theme areas
+                </p>
+              </div>
+            </div>
+          </section>
+
+          <section className="container py-8">
+            <div className="grid grid-cols-12 py-4">
+              <div className="col-span-12 md:col-span-3">
+                <h3 className="text-xl font-bold">Recipes</h3>
+                <ul className="pl-2 my-6 space-y-4 text-sm text-gray-500">
+                  <li>
+                    <a href="#">Morning Bliss Café</a>
+                  </li>
+
+                  <li>
+                    <a href="#">Sunrise Bites Kitchen</a>
+                  </li>
+
+                  <li>
+                    <a href="#">Brunch Haven Delights</a>
+                  </li>
+
+                  <li>
+                    <a href="#">Rise & Dine Eatery</a>
+                  </li>
+
+                  <li>
+                    <a href="#">Breakfast Oasis Junction</a>
+                  </li>
+                </ul>
+              </div>
+
+              <div className="col-span-12 md:col-span-9">
+                <div className="grid grid-cols-1 gap-6 my-8 sm:grid-cols-2 lg:grid-cols-3 justify-items-center">
+                  <div className="card">
+                    <img
+                      src="https://source.unsplash.com/-YHSwy6uqvk/300x160"
+                      className="rounded-md"
+                      alt=""
+                    />
+                    <h4 className="my-2">Chef John's Turkey Sloppy Joes</h4>
+                    <div className="flex justify-between py-2 text-xs text-gray-500">
+                      <span>⭐️ 5.0</span>
+                      <span>By: John Doe</span>
+                    </div>
+                  </div>
+
+                  <div className="card">
+                    <img
+                      src="https://source.unsplash.com/-YHSwy6uqvk/300x160"
+                      className="rounded-md"
+                      alt=""
+                    />
+                    <h4 className="my-2">Chef John's Turkey Sloppy Joes</h4>
+                    <div className="flex justify-between py-2 text-xs text-gray-500">
+                      <span>⭐️ 5.0</span>
+                      <span>By: John Doe</span>
+                    </div>
+                  </div>
+
+                  <div className="card">
+                    <img
+                      src="https://source.unsplash.com/-YHSwy6uqvk/300x160"
+                      className="rounded-md"
+                      alt=""
+                    />
+                    <h4 className="my-2">Chef John's Turkey Sloppy Joes</h4>
+                    <div className="flex justify-between py-2 text-xs text-gray-500">
+                      <span>⭐️ 5.0</span>
+                      <span>By: John Doe</span>
+                    </div>
+                  </div>
+
+                  <div className="card">
+                    <img
+                      src="https://source.unsplash.com/-YHSwy6uqvk/300x160"
+                      className="rounded-md"
+                      alt=""
+                    />
+                    <h4 className="my-2">Chef John's Turkey Sloppy Joes</h4>
+                    <div className="flex justify-between py-2 text-xs text-gray-500">
+                      <span>⭐️ 5.0</span>
+                      <span>By: John Doe</span>
+                    </div>
+                  </div>
+
+                  <div className="card">
+                    <img
+                      src="https://source.unsplash.com/-YHSwy6uqvk/300x160"
+                      className="rounded-md"
+                      alt=""
+                    />
+                    <h4 className="my-2">Chef John's Turkey Sloppy Joes</h4>
+                    <div className="flex justify-between py-2 text-xs text-gray-500">
+                      <span>⭐️ 5.0</span>
+                      <span>By: John Doe</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </main>
+      </body>
+    </div>
   );
 }

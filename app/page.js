3d6962d8f1b66d0e2b6c8a526fe87f62c06867dd
Tanmoy paul <84624966+tanmoypaul1005@users.noreply.github.com>
2024-04-28 22:11:49 +0600

    Refactor Home component to use grid layout and responsive styles

--- app/page.js
+++ app/page.js
@@ -14,25 +14,14 @@ export default function Home() {
             <div
               style={{
                 backgroundImage: "url('./assets/images/cover.png')",
-                padding: "1rem",
-                borderRadius: "1rem",
-                minHeight: "450px",
-                display: "grid",
-                alignItems: "center",
-                gridTemplateColumns: "repeat(12, 1fr)",
               }}
+              className="py-4 rounded-lg p-4 md:p-12 min-h-[450px] bg-cover grid place-items-center grid-cols-12"
             >
-              <div style={{ gridColumn: "span 12/span 6" }}>
-                <h1
-                  style={{
-                    fontWeight: "bold",
-                    fontSize: "3rem",
-                    color: "white",
-                  }}
-                >
+              <div className="col-span-12 md:col-span-6">
+                <h1 className="text-3xl font-bold text-white md:text-5xl">
                   Choose from thousands of recipes
                 </h1>
-                <p style={{ color: "white", margin: "1rem 0" }}>
+                <p className="my-4 text-white">
                   Appropriately integrate technically sound value with scalable
                   infomediaries negotiate sustainable strategic theme areas
                 </p>

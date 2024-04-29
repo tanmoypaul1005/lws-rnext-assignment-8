c2c8d83d7e7032884022ca03a34a7c629bc7ad92
Tanmoy paul <84624966+tanmoypaul1005@users.noreply.github.com>
2024-04-29 08:02:53 +0600

    Add async/await to RecipeList component to fetch recipes from API

--- app/components/RecipeList.js
+++ app/components/RecipeList.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import RecipeCard from './RecipeCard'
 
-const RecipeList = () => {
+const RecipeList =async () => {
+
+    const r=await fetch("http://localhost:3000/api/recipe")
+    .then((res)=>res.json())
+    console.log("r",r)
+
     return (
         <div className="col-span-12 md:col-span-9">
         <div className="grid grid-cols-1 gap-6 my-8 sm:grid-cols-2 lg:grid-cols-3 justify-items-center">

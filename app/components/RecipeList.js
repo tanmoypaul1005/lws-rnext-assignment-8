c1f0f70f2c900cb07f370b0056d747bcd79b6682
tanmoypaul007 <genie.tanmoypaul@gmail.com>
2024-04-29 22:39:32 +0600

    Refactor RecipeList component and fetch recipe data asynchronously

--- app/components/RecipeList.js
+++ app/components/RecipeList.js
@@ -1,29 +1,24 @@
+
 import { base_url } from '../utils/const'
 import RecipeCard from './RecipeCard'
 
-const RecipeList = ({ recipeList }) => {
+const RecipeList =async () => {
+
+    const recipeList=await fetch(base_url + "/recipe",{ cache: 'no-store' })
+    .then((res)=>res.json())
+    
+
     return (
         <div className="col-span-12 md:col-span-9">
-            <div className="grid grid-cols-1 gap-6 my-8 sm:grid-cols-2 lg:grid-cols-3 justify-items-center">
-                {
-                    recipeList?.data?.map((item,index)=>(
-                        <RecipeCard recipe={item} key={index} />
-                    ))
-                }
-            </div>
+        <div className="grid grid-cols-1 gap-6 my-8 sm:grid-cols-2 lg:grid-cols-3 justify-items-center">
+          {
+            recipeList?.data?.map((item,index)=>(
+              <RecipeCard recipe={item} key={index} />
+            ))
+          }
         </div>
+      </div>
     )
-}
-
-export async function getServerSideProps() {
-    const response = await fetch(base_url + "/recipe",{ cache: 'no-store' })
-    const recipeList = await response.json()
-
-    return {
-        props: {
-            recipeList
-        }
-    }
 }
 
 export default RecipeList
